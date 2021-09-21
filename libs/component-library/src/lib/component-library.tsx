f2e041385503a48b5bcc0b3a6d0a50d6c0124f4e
Andrea Caldera <andrea.caldera@gmail.com>
2021-09-21 20:21:02 +0100

    component library workflow

--- libs/component-library/src/lib/component-library.tsx
+++ libs/component-library/src/lib/component-library.tsx
@@ -5,7 +5,7 @@ const StyledComponentLibrary = styled.div`
 `;
 
 export function ComponentLibrary() {
-  console.log(1);
+  console.log(2);
   return (
     <StyledComponentLibrary>
       <h1>Welcome to ComponentLibrary!</h1>
