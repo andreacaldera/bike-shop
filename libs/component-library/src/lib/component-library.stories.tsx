f1f2c0b6faa7e9b6c5b61cdaef43ccdd2a555ce0
Andrea Caldera <andrea.caldera@gmail.com>
2021-09-21 20:26:27 +0100

    component library workflow

--- libs/component-library/src/lib/component-library.stories.tsx
+++ libs/component-library/src/lib/component-library.stories.tsx
@@ -1,14 +1,12 @@
 import { Story, Meta } from '@storybook/react';
-import { ComponentLibrary, ComponentLibraryProps } from './component-library';
+import { ComponentLibrary } from './component-library';
 
 export default {
   component: ComponentLibrary,
   title: 'ComponentLibrary',
 } as Meta;
 
-const Template: Story<ComponentLibraryProps> = (args) => (
-  <ComponentLibrary {...args} />
-);
+const Template: Story = (args) => <ComponentLibrary {...args} />;
 
 export const Primary = Template.bind({});
 Primary.args = {};
